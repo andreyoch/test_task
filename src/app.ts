5093633ce91713221807437f65e585597480b5f8
Andrzej Ciszewski <aw.ciszewski@protonmail.com>
2024-02-20 21:30:02 +0200

    feature: add clustering functionality

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 import express, { Request, Response } from 'express';
+import cluster, { Worker } from 'node:cluster';
+import os from 'node:os';
 import 'dotenv/config';
-const app: express.Application = express();
-const port: number = parseInt(process.env.SERVER_PORT || '3000');
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+if (cluster.isPrimary) {
+  const numCPUs = os.cpus().length;
+  console.log(`Master process with id ${process.pid} is running`);
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker: Worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  const app: express.Application = express();
+  const port: number = parseInt(process.env.SERVER_PORT || '3000');
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  });
+}
